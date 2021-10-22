ac8d61f74bd8c9bc0fd43dd3919d4f35b5539e6e
Alberto Rivai <arivai@Albertos-MBP.localdomain>
2021-10-22 16:15:12 +1100

    First commit

---
+++ aws_vars.tf
@@ -0,0 +1,105 @@
+provider "aws" {
+  access_key = ""
+  secret_key = ""
+  token      = var.aws_session_token
+  region     = var.aws_region
+}
+
+variable "aws_session_token" {
+  description = "Temporary session token used to create instances"
+}
+
+variable "aws_region" {
+  default = "ap-southeast-2"
+}
+
+variable "AZ" {
+  default = "ap-southeast-2a"
+}
+
+variable "VPCName" {
+  default = "student-name-vpc"
+}
+
+variable "VPCCIDR" {
+  default = "10.x.0.0/16"
+}
+
+variable "MGTCIDR_Block" {
+  default = "10.x.0.0/24"
+}
+
+variable "PublicCIDR_Block" {
+  type = list
+  default = [
+    {
+      ip = "10.x.1.0/24"
+      name = "untrust-1"
+    },
+    {
+      ip = "10.x.2.0/24"
+      name = "untrust-2"
+    }
+  ]
+}
+
+variable "PrivateCIDR_Block" {
+  default = "10.x.3.0/24"
+}
+
+variable "ServerKeyName" {
+  default = "awskeypair"
+}
+
+variable "fw_instance_size" {
+  default = "m5.xlarge"
+}
+
+variable "MasterS3Bucket" {
+  default = ""
+}
+
+variable "instance_profile" {
+  default = ""
+}
+  
+variable "PANFWRegionAMIID" {
+  type = map(string)
+  default = {
+    "us-west-2"      = "ami-019b369b2201d17e1"
+    "ap-northeast-1" = "ami-0ca2e94970201db8d"
+    "us-west-1"      = "ami-08e82ba0784b4e5ac"
+    "ap-northeast-2" = "ami-0a72f886dd8026c05"
+    "ap-southeast-1" = "ami-046798cef2cb2209e"
+    "ap-southeast-2" = "ami-02aed591c524aed18"
+    "eu-central-1"   = "ami-087597cf0637e3b39"
+    "eu-west-1"      = "ami-08e82ba0784b4e5ac"
+    "eu-west-2"      = "ami-0cea9a41443754f56"
+    "sa-east-1"      = "ami-05a40658314a5dc0f"
+    "us-east-1"      = "ami-050725600cf371a1c"
+    "us-east-2"      = "ami-0340ae9cf0a892bb9"
+    "ca-central-1"   = "ami-0d179c6cdc2589b25"
+    "ap-south-1"     = "ami-001923acdc459e458"
+  }
+}
+
+variable "UbuntuRegionMap" {
+  type = map(string)
+  default = {
+    "us-west-2"      = "ami-efd0428f"
+    "ap-northeast-1" = "ami-afb09dc8"
+    "us-west-1"      = "ami-2afbde4a"
+    "ap-northeast-2" = "ami-66e33108"
+    "ap-southeast-1" = "ami-8fcc75ec"
+    "ap-southeast-2" = "ami-96666ff5"
+    "eu-central-1"   = "ami-060cde69"
+    "eu-west-1"      = "ami-a8d2d7ce"
+    "eu-west-2"      = "ami-f1d7c395"
+    "sa-east-1"      = "ami-4090f22c"
+    "us-east-1"      = "ami-80861296"
+    "us-east-2"      = "ami-618fab04"
+    "ca-central-1"   = "ami-b3d965d7"
+    "ap-south-1"     = "ami-c2ee9dad"
+  }
+}
+
---
+++ main.tf
@@ -0,0 +1,281 @@
+resource "aws_vpc" "main" {
+  cidr_block = var.VPCCIDR
+  tags = {
+    Name = var.VPCName
+  }
+}
+
+
+#Create the 1 management, 2 public and 1 private subnet
+resource "aws_subnet" "NewMGTSubnet" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.MGTCIDR_Block
+  availability_zone = var.AZ
+  tags = {
+    Name = "MGT"
+  }
+}
+
+resource "aws_subnet" "NewPublicSubnet" {
+  count             = "${length(var.PublicCIDR_Block)}"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "${lookup(element(var.PublicCIDR_Block, count.index), "ip")}"
+  availability_zone = var.AZ
+  tags = {
+    Name            = "${lookup(element(var.PublicCIDR_Block, count.index), "name")}"
+  }
+}
+
+resource "aws_subnet" "NewPrivateSubnet" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.PrivateCIDR_Block
+  availability_zone = var.AZ
+  tags = {
+    Name = "Private1"
+  }
+}
+
+#Create management, untrust and trust route 
+resource "aws_route_table" "management" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+  tags = {
+    Name = "mgtroute"
+  }
+}
+
+resource "aws_route_table" "untrust" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+  tags = {
+    Name = "Untrustroute1"
+  }
+}
+
+resource "aws_route_table" "trust" {
+  vpc_id = aws_vpc.main.id
+  depends_on = [
+    aws_vpc.main,
+    aws_network_interface.FWPrivate12NetworkInterface,
+  ]
+  route {
+    cidr_block           = "0.0.0.0/0"
+    network_interface_id = aws_network_interface.FWPrivate12NetworkInterface.id
+  }
+  tags = {
+    Name = "Trustroute1"
+  }
+}
+
+#Route table association
+resource "aws_route_table_association" "mgt" {
+  subnet_id      = aws_subnet.NewMGTSubnet.id
+  route_table_id = aws_route_table.management.id
+}
+
+resource "aws_route_table_association" "untrust" {
+  for_each = {
+    for k, v in aws_subnet.NewPublicSubnet : k => v
+  }
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.untrust.id
+}
+
+resource "aws_route_table_association" "trust" {
+  subnet_id      = aws_subnet.NewPrivateSubnet.id
+  route_table_id = aws_route_table.trust.id
+}
+
+#Create IGW
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "IGW1"
+  }
+}
+
+#Create Security Group
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+  description = "Allow all traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "Allow-all1"
+  }
+}
+
+#Create 1 management, 2 untrust and 1 trust firewall Interfaces
+resource "aws_network_interface" "FWManagementNetworkInterface" {
+  subnet_id         = aws_subnet.NewMGTSubnet.id
+  security_groups   = [aws_security_group.allow_all.id]
+  source_dest_check = false
+  tags = {
+    Name = "FWManagementNIC"
+  }
+}
+
+resource "aws_network_interface" "FWPublic1NetworkInterface" {
+  subnet_id         = aws_subnet.NewPublicSubnet[0].id
+  security_groups   = [aws_security_group.allow_all.id]
+  source_dest_check = false
+  tags = {
+    Name = "FWExternalNIC1"
+  }
+}
+
+resource "aws_network_interface" "FWPublic2NetworkInterface" {
+  subnet_id         = aws_subnet.NewPublicSubnet[1].id
+  security_groups   = [aws_security_group.allow_all.id]
+  source_dest_check = false
+  tags = {
+    Name = "FWExternalNIC2"
+  }
+}
+
+resource "aws_network_interface" "FWPrivate12NetworkInterface" {
+  subnet_id         = aws_subnet.NewPrivateSubnet.id
+  security_groups   = [aws_security_group.allow_all.id]
+  source_dest_check = false
+  private_ips_count = 0
+  tags = {
+    Name = "FWTrustNIC"
+  }
+}
+
+#Create 3 FW EIPs (management and 2 public interfaces)
+resource "aws_eip" "ManagementElasticIP" {
+  vpc = true
+  tags = {
+    Name = "ManagementEIP"
+  }
+}
+
+resource "aws_eip" "PublicElasticIP1" {
+  vpc = true
+  tags = {
+    Name = "PublicEIP1"
+  }
+}
+
+resource "aws_eip" "PublicElasticIP2" {
+  vpc = true
+  tags = {
+    Name = "PublicEIP2"
+  }
+}
+
+#Create firewall EIP association
+resource "aws_eip_association" "FWEIPManagementAssociation" {
+  network_interface_id = aws_network_interface.FWManagementNetworkInterface.id
+  allocation_id        = aws_eip.ManagementElasticIP.id
+}
+
+resource "aws_eip_association" "FWEIPPublicAssociation1" {
+  network_interface_id = aws_network_interface.FWPublic1NetworkInterface.id
+  allocation_id        = aws_eip.PublicElasticIP1.id
+}
+
+resource "aws_eip_association" "FWEIPPublicAssociation2" {
+  network_interface_id = aws_network_interface.FWPublic2NetworkInterface.id
+  allocation_id        = aws_eip.PublicElasticIP2.id
+}
+
+#Create Linux interface
+resource "aws_network_interface" "WPNetworkInterface" {
+  subnet_id         = aws_subnet.NewPrivateSubnet.id
+  security_groups   = [aws_security_group.allow_all.id]
+  source_dest_check = false
+  tags = {
+    Name = "HostNIC"
+  }
+}
+
+#Create FW instances
+resource "aws_instance" "FWInstance" {
+  disable_api_termination              = false
+  iam_instance_profile                 = var.instance_profile
+  instance_initiated_shutdown_behavior = "stop"
+  ebs_optimized                        = true
+  ami                                  = var.PANFWRegionAMIID[var.aws_region]
+  instance_type                        = var.fw_instance_size
+
+  ebs_block_device {
+    device_name           = "/dev/xvda"
+    volume_type           = "gp2"
+    delete_on_termination = true
+    volume_size           = 60
+  }
+
+  monitoring = false
+
+  network_interface {
+    device_index         = 0
+    network_interface_id = aws_network_interface.FWManagementNetworkInterface.id
+  }
+
+  network_interface {
+    device_index         = 1
+    network_interface_id = aws_network_interface.FWPublic1NetworkInterface.id
+  }
+
+    network_interface {
+    device_index         = 2
+    network_interface_id = aws_network_interface.FWPublic2NetworkInterface.id
+  }
+
+  network_interface {
+    device_index         = 3
+    network_interface_id = aws_network_interface.FWPrivate12NetworkInterface.id
+  }
+
+  user_data = base64encode(
+    join("", ["vmseries-bootstrap-aws-s3bucket=", var.MasterS3Bucket]),
+  )
+  tags = {
+    Name = "vmseriesFirewall"
+  }
+}
+
+#Create Linux instance
+resource "aws_instance" "WPWebInstance" {
+  disable_api_termination              = false
+  instance_initiated_shutdown_behavior = "stop"
+  ami                                  = var.UbuntuRegionMap[var.aws_region]
+  instance_type                        = "t2.micro"
+
+  key_name   = var.ServerKeyName
+  monitoring = false
+
+  network_interface {
+    device_index         = 0
+    network_interface_id = aws_network_interface.WPNetworkInterface.id
+  }
+  tags = {
+    Name = "Linuxhost"
+    Type = "Dev-servers"
+  }
+}
+
